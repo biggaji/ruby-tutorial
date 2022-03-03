2e6baeebcbb5bd5645bcb7714f0d32c617db8746
biggaji <herityjohnny14@gmail.com>
2022-03-03 11:25:15 +0100

    learned how to create a new folder via the Dir command

---
+++ directory.rb
@@ -0,0 +1,9 @@
+puts Dir.pwd  # this would print the current working directory
+
+# to create a new folder
+
+Dir.mkdir "testDirectory"
+
+# to check if a directory or folder exists
+
+puts Dir.exists? "testDirectory"
